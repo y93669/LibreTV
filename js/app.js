78c1ca0c8c650a6d52eb48e5884d3b20c1f34a25
y93669 <114243660+y93669@users.noreply.github.com>
2025-08-09 16:31:17 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -584,11 +584,11 @@ function resetSearchArea() {
     try {
         window.history.pushState(
             {},
-            `LibreTV - 免费在线视频搜索与观看平台`,
+            `蓝果STV - 免费在线视频搜索与观看平台`,
             `/`
         );
         // 更新页面标题
-        document.title = `LibreTV - 免费在线视频搜索与观看平台`;
+        document.title = `蓝果STV - 免费在线视频搜索与观看平台`;
     } catch (e) {
         console.error('更新浏览器历史失败:', e);
     }
